8c6073f26b2867354a7e8e08076c0b2113cebe3e
Christian Joudrey <cmallette@gmail.com>
2011-04-09 01:30:24 -0400

    Added example to add custom helpers

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-require('typhoon')(__dirname, require('./configs'));
+// Require Typhoon
+var typhoon = require('typhoon');
+
+// Add custom helpers
+// typhoon.Helpers.hello = function() { return 'world'; };
+
+// Start server
+typhoon(__dirname, require('./configs'));
